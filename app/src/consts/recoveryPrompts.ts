7ffa2bdd62e923bb9598becd94f8ef1a75e19a6c
Justin Hernandez <justin.hernandez@self.xyz>
2025-10-09 15:00:16 -0700

    fix header

--- app/src/consts/recoveryPrompts.ts
+++ app/src/consts/recoveryPrompts.ts
@@ -9,9 +9,6 @@
 export type RecoveryPromptAllowedRoute =
   (typeof RECOVERY_PROMPT_ALLOWED_ROUTES)[number];
 
-// SPDX-FileCopyrightText: 2025 Social Connect Labs, Inc.
-// SPDX-License-Identifier: BUSL-1.1
-// NOTE: Converts to Apache-2.0 on 2029-06-11 per LICENSE.
 /**
  * Screens where we intentionally show the recovery reminder. This allow list is
  * intentionally short so that new product surfaces do not accidentally inherit
